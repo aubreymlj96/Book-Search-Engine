31b6290a89f858f46c56bd4062539c9a1282d587
aubrey johnson <aubreymlj96@gmail.com>
2024-01-28 16:37:25 -0500

    added initial code for typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,41 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    books: [Book]!
+  }
+
+  type Book {
+    _id: ID
+    authors: String
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    book(username: String): [Book]
+    book(bookId: ID!): Book
+    me: User
+  }
+
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(description: String!): Book
+    deleteBook(bookId: ID!, userId: ID!): Book
+  }
+`;
+
+module.exports = typeDefs;
